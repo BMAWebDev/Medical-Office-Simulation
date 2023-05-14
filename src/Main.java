78f8c60d9ba13e6759f57b5a5451b72622be326d
Bardita Andrei <bmihaiandrei@gmail.com>
2023-05-14 19:29:59 +0300

    added Person class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class DB {
 	private final String DB_URL;
@@ -48,6 +50,54 @@ class DB {
 		Statement stmt = con.createStatement();
 
 		return stmt.executeUpdate(sqlQuery);
+	}
+}
+
+class Person {
+	private String first_name, last_name, phone_number;
+
+	public Person(String _first_name, String _last_name, String _phone_number) {
+		this.first_name = _first_name;
+		this.last_name = _last_name;
+		this.phone_number = _phone_number;
+	}
+
+	public String getLastName() {
+		return last_name;
+	}
+
+	public void setLastName(String _last_name) {
+		this.last_name = _last_name;
+	}
+
+	public String getFirstName() {
+		return first_name;
+	}
+
+	public void setFirstName(String _first_name) {
+		this.first_name = _first_name;
+	}
+
+	public String getPhoneNumber() {
+		return phone_number;
+	}
+
+	public void setPhoneNumber(String _phone_number) {
+		this.phone_number = _phone_number;
+	}
+
+	public String getName() {
+		return String.format("%s %s", first_name, last_name);
+	}
+
+	public Map<String, String> getPersonalInfo() {
+		Map<String, String> personalInfo = new HashMap<>();
+
+		personalInfo.put("first_name", first_name);
+		personalInfo.put("last_name", last_name);
+		personalInfo.put("phone_number", phone_number);
+		
+		return personalInfo;
 	}
 }
 
