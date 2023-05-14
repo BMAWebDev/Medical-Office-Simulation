e8d53cc3f3f28ad24ffa3e64844897020839150b
Bardita Andrei <bmihaiandrei@gmail.com>
2023-05-14 22:03:42 +0300

    added Client class + started work on CLI Menu

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 enum DoctorRank {Nurse, FamilyDoctor, Surgeon}
+
+enum MenuName {MainMenu, Appointment1, Appointment2, ConfirmCancel}
 
 class DB {
 	private final String DB_URL;
@@ -179,8 +182,89 @@ class Medic extends Person {
 	}
 }
 
+class Client extends Person {
+	private final LocalDate date_of_birth;
+	public int client_id;
+	private String email, cnp, address;
+
+	public Client(int _client_id, String _first_name, String _last_name, String _phone_number, String _cnp, LocalDate _date_of_birth, String _address) {
+		super(_first_name, _last_name, _phone_number);
+
+		this.client_id = _client_id;
+		this.date_of_birth = _date_of_birth;
+		this.cnp = _cnp;
+		this.address = _address;
+	}
+
+	public Client(int _client_id, String _first_name, String _last_name, String _phone_number, String _cnp, LocalDate _date_of_birth, String _address, String _email) {
+		super(_first_name, _last_name, _phone_number);
+
+		this.client_id = _client_id;
+		this.date_of_birth = _date_of_birth;
+		this.cnp = _cnp;
+		this.address = _address;
+		this.email = _email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String _email) {
+		this.email = _email;
+	}
+
+	public String getCNP() {
+		return cnp;
+	}
+
+	public void setCNP(String _cnp) {
+		this.cnp = _cnp;
+	}
+
+	@Override
+	public Map<String, String> getPersonalInfo() {
+		Map<String, String> personalInfo = super.getPersonalInfo();
+
+		personalInfo.put("client_id", Integer.toString(this.client_id));
+		personalInfo.put("date_of_birth", this.date_of_birth.toString());
+		personalInfo.put("address", this.address);
+		personalInfo.put("cnp", this.cnp);
+		personalInfo.put("email", this.email);
+
+		return personalInfo;
+	}
+}
+
+class Menu {
+	private MenuName menuName;
+	private String question;
+	private String[] answers;
+
+	public Menu() {
+		this.menuName = MenuName.MainMenu;
+	}
+
+	public Menu(MenuName _name) {
+		this.menuName = _name;
+	}
+
+	private String getAnswers() {
+		return String.join("\n", answers);
+	}
+}
+
 public class Main {
 	public static void main(String[] args) throws SQLException {
 		DB db = new DB();
+		Scanner scanner = new Scanner(System.in);
+		String input;
+
+		do {
+			System.out.println("Enter a number from the list below in order to continue, or 'stop' to exit.");
+			input = scanner.nextLine();
+		} while (!input.equalsIgnoreCase("stop"));
+
+		scanner.close();
 	}
 }
